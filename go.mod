ce8e0973f5dcbc33dfc73aa6816fb2817669baca
John Sigvald Skauge <thejohnskauge@gmail.com>
2020-08-21 10:36:30 +0200

    Simple return added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module bitbucket.org/tv2norge/sudo-gometrics-demo
 
 go 1.15
 
-require github.com/gorilla/mux v1.7.4 // indirect
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/prometheus/common v0.13.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"net/http"
 
+	"github.com/gorilla/mux"
+	"github.com/prometheus/common/log"
+)
+
+type Data struct {
+	Id      int
+	Name    string
+	Message string
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	data := Data{1024, "First post", "This is the first post"}
+	js, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Server", "Ein slek der Mix Master")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+	// fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", rootHandler)
+
+	log.Info(http.ListenAndServe(":8000", r))
 }
