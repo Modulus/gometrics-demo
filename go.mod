ad1db1aa02b4b8a270c7fbabbd9c493d9f6512d4
John Sigvald Skauge <thejohnskauge@gmail.com>
2020-08-24 09:52:48 +0200

    Added prometheus libs

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.7.4
+	github.com/prometheus/client_golang v1.7.1
 	github.com/prometheus/common v0.13.0
 )
--- main.go
+++ main.go
@@ -2,21 +2,40 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/prometheus/common/log"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type Data struct {
-	Id      int
-	Name    string
-	Message string
+	TimeStamp string
+	Name      string
+	Message   string
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
+func VoteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	category := vars["category"]
+	log.Println("You voted for " + category)
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	data := Data{currentTime, "You voted for", category}
+	js, err := json.Marshal(data)
 
-	data := Data{1024, "First post", "This is the first post"}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Server", "Ein slek der Mix Master")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	data := Data{currentTime, "First post", "This is the first post"}
 	js, err := json.Marshal(data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -25,12 +44,15 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Server", "Ein slek der Mix Master")
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
-	// fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", rootHandler)
+	r.HandleFunc("/", RootHandler)
+	r.HandleFunc("/vote/{category}", VoteHandler)
+	log.Println("Server running on 0.0.0.0:8000")
 
-	log.Info(http.ListenAndServe(":8000", r))
+	http.Handle("/metrics", promhttp.Handler())
+
+	http.ListenAndServe(":8000", r)
 }
